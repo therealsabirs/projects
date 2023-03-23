eb694c568c5855469275b4cf092d40a3d3e465c4
Sabir Shaikh <44839291+therealsabirs@users.noreply.github.com>
2023-03-23 15:34:55 +0530

    Add files via upload

---
+++ Quick Backup/Quick Backup/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Quick_Backup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(58, 140);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(49, 17);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Copy";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(125, 140);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(52, 17);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Move";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(75, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(42, 108);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Select Destination Folder";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(75, 28);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(66, 17);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Desktop";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(75, 51);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(74, 17);
+            this.checkBox2.TabIndex = 14;
+            this.checkBox2.Text = "Download";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(75, 74);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(80, 17);
+            this.checkBox3.TabIndex = 15;
+            this.checkBox3.Text = "Documents";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(245, 244);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Quick Backup";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ Quick Backup/Quick Backup/Form1.cs
@@ -0,0 +1,406 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quick_Backup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+
+        }
+        string sourcefolderPath = null;
+        string destinationPath = null;
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (radioButton1.Checked)
+            {
+                if (!checkBox1.Checked && !checkBox2.Checked && !checkBox3.Checked)
+                {
+                    MessageBox.Show("Select atleast any one option");
+                }
+                //Desktop Copy
+                if (checkBox1.Checked)
+                {
+                    sourcefolderPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                    if (destinationPath != null)
+                    {
+                        string sourceDir = sourcefolderPath;
+                        string destDir = destinationPath;
+                        string[] files = Directory.GetFiles(sourceDir, "*", SearchOption.AllDirectories);
+                        string[] subDirs = Directory.GetDirectories(sourceDir, "*", SearchOption.AllDirectories);
+
+                        // Copy the files
+                        foreach (string file in files)
+                        {
+                            string relPath = file.Substring(sourceDir.Length + 1);
+                            string destFile = Path.Combine(destDir, relPath);
+                            Directory.CreateDirectory(Path.GetDirectoryName(destFile));
+                            File.Copy(file, destFile, true);
+                        }
+
+                        // Copy the directories
+                        foreach (string subDir in subDirs)
+                        {
+                            string relPath = subDir.Substring(sourceDir.Length + 1);
+                            string destSubDir = Path.Combine(destDir, relPath);
+                            Directory.CreateDirectory(destSubDir);
+                        }
+                        MessageBox.Show("data copied successfully from-" + sourcefolderPath + " to-" + destinationPath);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Select Destination Folder");
+                    }
+                }
+                //Download Copy
+                if (checkBox2.Checked)
+                {
+                    sourcefolderPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads";
+                    if (destinationPath != null)
+                    {
+
+                        string sourceDir = sourcefolderPath;
+                        string destDir = destinationPath;
+                        string[] files = Directory.GetFiles(sourceDir, "*", SearchOption.AllDirectories);
+                        string[] subDirs = Directory.GetDirectories(sourceDir, "*", SearchOption.AllDirectories);
+
+                        // Copy the files
+                        foreach (string file in files)
+                        {
+                            string relPath = file.Substring(sourceDir.Length + 1);
+                            string destFile = Path.Combine(destDir, relPath);
+                            Directory.CreateDirectory(Path.GetDirectoryName(destFile));
+                            File.Copy(file, destFile, true);
+                        }
+
+                        // Copy the directories
+                        foreach (string subDir in subDirs)
+                        {
+                            string relPath = subDir.Substring(sourceDir.Length + 1);
+                            string destSubDir = Path.Combine(destDir, relPath);
+                            Directory.CreateDirectory(destSubDir);
+                        }
+                        MessageBox.Show("data copied successfully from-" + sourcefolderPath + " to-" + destinationPath);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Select Destination Folder");
+                    }
+                }
+                //Documents Copy
+                if (checkBox3.Checked)
+                {
+                    sourcefolderPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                    if (destinationPath != null)
+                    {
+                        string sourceDir = sourcefolderPath;
+                        string destDir = destinationPath;
+                        string[] files = Directory.GetFiles(sourceDir, "*", SearchOption.AllDirectories);
+                        string[] subDirs = Directory.GetDirectories(sourceDir, "*", SearchOption.AllDirectories);
+
+                        // Copy the files
+                        foreach (string file in files)
+                        {
+                            string relPath = file.Substring(sourceDir.Length + 1);
+                            string destFile = Path.Combine(destDir, relPath);
+                            Directory.CreateDirectory(Path.GetDirectoryName(destFile));
+                            File.Copy(file, destFile, true);
+                        }
+
+                        // Copy the directories
+                        foreach (string subDir in subDirs)
+                        {
+                            string relPath = subDir.Substring(sourceDir.Length + 1);
+                            string destSubDir = Path.Combine(destDir, relPath);
+                            Directory.CreateDirectory(destSubDir);
+                        }
+                        MessageBox.Show("data copied successfully from-" + sourcefolderPath + " to-" + destinationPath);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Select Destination Folder");
+                    }
+                }
+            }
+            else if (radioButton2.Checked)
+            {
+
+                if (!checkBox1.Checked && !checkBox2.Checked && !checkBox3.Checked)
+                {
+                    MessageBox.Show("Select atleast any one option");
+                }
+                //Desktop move
+                if (checkBox1.Checked)
+                {
+                    try
+                    {
+                        sourcefolderPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                        if (destinationPath != null && sourcefolderPath != null)
+                        {
+
+
+                            string sourceDir = sourcefolderPath;
+                            string destDir = destinationPath;
+                            string[] files = Directory.GetFiles(sourceDir, "*", SearchOption.AllDirectories);
+                            string[] subDirs = Directory.GetDirectories(sourceDir, "*", SearchOption.AllDirectories);
+
+                            // Copy the files
+                            foreach (string file in files)
+                            {
+                                string relPath = file.Substring(sourceDir.Length + 1);
+                                string destFile = Path.Combine(destDir, relPath);
+                                Directory.CreateDirectory(Path.GetDirectoryName(destFile));
+                                File.Copy(file, destFile, true);
+                                File.Delete(file);
+                            }
+
+                            // Copy the directories
+                            foreach (string subDir in subDirs)
+                            {
+                                string relPath = subDir.Substring(sourceDir.Length + 1);
+                                string destSubDir = Path.Combine(destDir, relPath);
+                                Directory.CreateDirectory(destSubDir);
+                               // Directory.Delete(subDir);
+                            }
+                            foreach (string subDir in subDirs)
+                            {
+                                if (Directory.GetFiles(subDir).Length == 0 && Directory.GetDirectories(subDir).Length == 0)
+                                {
+                                    Directory.Delete(subDir);
+                                }
+                            }
+
+                            MessageBox.Show("data moved successfully from-" + sourcefolderPath + " to-" + destinationPath);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Select Destination Folder");
+                        }
+                    }catch(Exception ex)
+                    {
+                        MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                //Download Move
+                if (checkBox2.Checked)
+                {
+                    try
+                    {
+                        sourcefolderPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads";
+                        if (destinationPath != null && sourcefolderPath != null)
+                        {
+
+
+                            string sourceDir = sourcefolderPath;
+                            string destDir = destinationPath;
+                            string[] files = Directory.GetFiles(sourceDir, "*", SearchOption.AllDirectories);
+                            string[] subDirs = Directory.GetDirectories(sourceDir, "*", SearchOption.AllDirectories);
+
+                            // Copy the files
+                            foreach (string file in files)
+                            {
+                                string relPath = file.Substring(sourceDir.Length + 1);
+                                string destFile = Path.Combine(destDir, relPath);
+                                Directory.CreateDirectory(Path.GetDirectoryName(destFile));
+                                File.Copy(file, destFile, true);
+                                File.Delete(file);
+                            }
+
+                            // Copy the directories
+                            foreach (string subDir in subDirs)
+                            {
+                                string relPath = subDir.Substring(sourceDir.Length + 1);
+                                string destSubDir = Path.Combine(destDir, relPath);
+                                Directory.CreateDirectory(destSubDir);
+                                //Directory.Delete(subDir);
+                            }
+                            foreach (string subDir in subDirs)
+                            {
+                                if (Directory.GetFiles(subDir).Length == 0 && Directory.GetDirectories(subDir).Length == 0)
+                                {
+                                    Directory.Delete(subDir);
+                                }
+                            }
+
+                            MessageBox.Show("data moved successfully from-" + sourcefolderPath + " to-" + destinationPath);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Select Destination Folder");
+                        }
+                    }catch(Exception ex)
+                    {
+                        MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                //Documents Move
+                if (checkBox3.Checked)
+                {
+                    sourcefolderPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                    try
+                    {
+                        if (destinationPath != null && sourcefolderPath != null)
+                        {
+
+
+                            string sourceDir = sourcefolderPath;
+                            string destDir = destinationPath;
+                            string[] files = Directory.GetFiles(sourceDir, "*", SearchOption.AllDirectories);
+                            string[] subDirs = Directory.GetDirectories(sourceDir, "*", SearchOption.AllDirectories);
+
+                            // Copy the files
+                            foreach (string file in files)
+                            {
+                                string relPath = file.Substring(sourceDir.Length + 1);
+                                string destFile = Path.Combine(destDir, relPath);
+                                Directory.CreateDirectory(Path.GetDirectoryName(destFile));
+                                File.Copy(file, destFile, true);
+                                File.Delete(file);
+                            }
+
+                            // Copy the directories
+                            foreach (string subDir in subDirs)
+                            {
+                                string relPath = subDir.Substring(sourceDir.Length + 1);
+                                string destSubDir = Path.Combine(destDir, relPath);
+                                Directory.CreateDirectory(destSubDir);
+                                //Directory.Delete(subDir);
+                            }
+                            foreach (string subDir in subDirs)
+                            {
+                                if (Directory.GetFiles(subDir).Length == 0 && Directory.GetDirectories(subDir).Length == 0)
+                                {
+                                    Directory.Delete(subDir);
+                                }
+                            }
+                            MessageBox.Show("data moved successfully from-" + sourcefolderPath + " to-" + destinationPath);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Select Destination Folder");
+                        }
+                    }catch(Exception ex)
+                    {
+                        MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                }
+
+
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Select Copy or Move");
+            }
+            ClearForm();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            folderBrowserDialog1.Description = "Select a folder.";
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string folderPath = folderBrowserDialog1.SelectedPath;
+                // Do something with the folder path...
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            folderBrowserDialog1.Description = "Select a folder to move data.";
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                sourcefolderPath = folderBrowserDialog1.SelectedPath;
+                // Do something with the folder path...
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            folderBrowserDialog1.Description = "Select a folder to move data.";
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                destinationPath = folderBrowserDialog1.SelectedPath;
+                // Do something with the folder path...
+            }
+            button3.BackColor = Color.Gray;
+            button3.ForeColor = Color.White;
+        }
+        private void ClearForm()
+        {
+            foreach (Control c in Controls)
+            {
+               
+                button3.ForeColor = Color.Black;
+              
+                button3.BackColor = Color.White;
+
+                if (c is TextBox)
+                {
+                    ((TextBox)c).Text = "";
+                }
+                else if (c is ComboBox)
+                {
+                    ((ComboBox)c).SelectedIndex = -1;
+                }
+                else if (c is CheckBox)
+                {
+                    ((CheckBox)c).Checked = false;
+                }
+                else if (c is RadioButton)
+                {
+                    ((RadioButton)c).Checked = false;
+                }
+                // Add other control types as needed
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
